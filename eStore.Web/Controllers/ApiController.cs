ff105ef6ae76b7951a4fb96ab73f5d39c8904c15
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-12-03 23:55:41 +0600

    Update ApiController.cs

--- eStore.Web/Controllers/ApiController.cs
+++ eStore.Web/Controllers/ApiController.cs
@@ -7,15 +7,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace eStore.Web.Controllers
-{
-    
+namespace eStore.Web.Controllers {
     [ApiController]
     [Route("api/v{version:apiVersion}/[controller]")]
-    public abstract class ApiController : ControllerBase
-    {
+    public abstract class ApiController : ControllerBase {
         private IMediator _mediator;
-
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
