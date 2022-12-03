a9fc4f032623babc5fb6f1f4e21dbd830ae4af5d
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-12-03 23:54:15 +0600

    Update IdentityResultExtensions.cs

--- eStore.Infrastructure.Identity/Extensions/IdentityResultExtensions.cs
+++ eStore.Infrastructure.Identity/Extensions/IdentityResultExtensions.cs
@@ -5,12 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace eStore.Infrastructure.Identity.Extensions
-{
+namespace eStore.Infrastructure.Identity.Extensions {
     public static class IdentityResultExtensions
     {
-        public static Result<string> ToApplicationResult(this IdentityResult result, string message, string data)
-        {
+        public static Result<string> ToApplicationResult(this IdentityResult result, string message, string data) {
             return result.Succeeded
                 ? Result<string>.Success(message, data)
                 : Result<string>.Failure(result.Errors.Select(e => e.Description));
